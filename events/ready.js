d87f8056c85ac83de4654f2bf2eb2bc9eb665037
AH_REM <m3gatrore@gmail.com>
2020-05-02 20:06:54 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 'use_strict';
 
+const settings = require('../settings.json');
 const emoteSearch = require('../utils/emoteSearch');
 
 /**
@@ -12,13 +13,22 @@ module.exports = client => {
     const log = `${emoteSearch('green_arrow')} bot gave reboot.`;
     client.sendLog('boot', log);
 
+    // Set interval to refresh bot presence
     client.setPresence = () => require('../utils/setPresence')(client);
     client.setPresence();
     setInterval(() => client.setPresence(), 300000); // 5min
 
-    require('../roles/addRoleMember').refresh(client);
-    // require('../roles/addRoleColor').refresh(client);
+    const guild = client.guildlist.get('zone_dev');
 
-    require('../utils/roleLoader')(client);
+    // Refresh message to channel 'regles'
+    const channel = guild.channels.get('regles');
+    client.channels.cache.get(channel.id).messages.fetch(channel.messageId).then(m => m.react('✅'));
+
+    // And channel 'choisis-ta-couleur'
+    const channel2 = guild.channels.get('choisis-ta-couleur');
+    client.channels.cache.get(channel2.id).messages.fetch(channel2.messageId).then(m => m.react('595226083800907786')); // id emote : ":0000ff:"
+
+    // Get role Member
+    client.roleMember = client.guilds.cache.get(guild.id).roles.cache.find(role => role.name === settings.role.member);
 
 };
